1c487d4fb07b2dc3da7ec491e6edfbc177872f55
Parag <prgkhan@gmail.com>
2024-04-09 17:20:59 +0600

    added vouchers

---
+++ app/Http/Controllers/VoucherController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voucher;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreVoucherRequest;
+use App\Http\Requests\UpdateVoucherRequest;
+
+class VoucherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $vouchers = Voucher::paginate(20);
+
+        return response()->json($vouchers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVoucherRequest $request)
+    {
+        $voucher = Voucher::create($request->validated());
+
+        $voucher->addMediaFromRequest('image')->toMediaCollection();
+
+        return response()->json($voucher);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Voucher $voucher)
+    {
+        return response()->json($voucher);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVoucherRequest $request, Voucher $voucher)
+    {
+        $voucher->update($request->validated());
+
+        if ($request->has('image')) {
+            $voucher->clearMediaCollection();
+            $voucher->addMediaFromRequest('image')->toMediaCollection();
+        }
+
+        return response()->json($voucher);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Voucher $voucher)
+    {
+        $voucher->delete();
+
+        return response()->json($voucher);
+    }
+}
