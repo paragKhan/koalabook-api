802710b5420ffc8cc43ce49cf0a3bcdaffd9f4a3
Parag <prgkhan@gmail.com>
2024-04-16 01:15:12 +0600

    fix: thumb generated at 250px

--- app/Models/ColoringBook.php
+++ app/Models/ColoringBook.php
@@ -30,7 +30,7 @@ class ColoringBook extends Model implements HasMedia
     public function registerMediaConversions(Media $media = null): void
     {
         $this->addMediaConversion('thumb')
-            ->width(100);
+            ->width(250);
     }
 
     protected function getImageAttribute(){
